3a91c3e8d783b75d132352114b163416fbc71a87
Umair Khan <omerjerk@gmail.com>
2014-10-13 22:46:59 +0530

    Add relevant JNI code
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

---
+++ app/jni/FlappyAutomator.cpp
@@ -0,0 +1,67 @@
+#include <jni.h>
+
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/video/tracking.hpp>
+
+using namespace cv;
+
+class ContentFinder {
+    private:
+    	int histSize[2];
+        const float* ranges[2];
+        int channels[2];
+        float hranges[2];
+        float sranges[2];
+    public:
+    	ContentFinder() {
+            // Prepare arguments for a color histogram
+            histSize[0] = 180;
+            histSize[1] = 256;
+            hranges[0] = 0; hranges[1] = 180;
+            sranges[0] = 0; sranges[1] = 256;
+            ranges[0] = hranges;
+            ranges[1] = sranges;
+            channels[0] = 0; // the three channels
+            channels[1] = 1;
+        }
+
+        MatND getHistogram(const Mat& image) {
+        	MatND hist;
+        	calcHist(&image, 1, channels, Mat(), hist, 2, histSize, ranges, true, false);
+        	return hist;
+        }
+
+        Mat backProject (const Mat& image, const MatND& histogram) {
+        	Mat result;
+        	normalize(histogram, histogram, 0, 255, NORM_MINMAX, -1, Mat());
+        	calcBackProject(&image, 1, channels, histogram, result, ranges, 1);
+        	threshold(result, result, 5, 255, 0);
+        	return result;
+        }
+
+        void getMeanShift(Mat& result, Rect& track_window) {
+        	TermCriteria criteria(TermCriteria::MAX_ITER, 10, 0.01);
+        	meanShift(result, track_window, criteria);
+        }
+
+        int findHeadCoord (Mat& result) {
+        	uchar *data;
+        	int rows = result.rows;
+        	int elemSize = result.elemSize();
+        	for (int i = 0; i < rows; ++i) {
+        		data = result.data + i*result.step + 88*elemSize;
+        		if (*data == 255) {
+        			return i;
+        		}
+        	}
+
+        	return 0;
+        }
+};
+
+JNIEXPORT int JNICALL
+Java_in_omerjerk_flappybirdautomator_someFunction(JNIEnv * env, jobject  obj) {
+    return 0;
+}
---
+++ app/obj/local/armeabi-v7a/objs/flappy_automator/FlappyAutomator.o.d
@@ -0,0 +1,152 @@
+/home/omerjerk/AndroidstudioProjects/FlappyBirdAutomator/app/obj/local/armeabi-v7a/objs/flappy_automator/FlappyAutomator.o: \
+ /home/omerjerk/AndroidstudioProjects/FlappyBirdAutomator/app/jni/FlappyAutomator.cpp \
+ /home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/jni.h \
+ /home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ /home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ /home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/android/api-level.h \
+ /home/omerjerk/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/highgui/highgui.hpp \
+ /home/omerjerk/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp \
+ /home/omerjerk/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h \
+ /home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/assert.h \
+ /home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/stdlib.h \
+ /home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/string.h \
+ /home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/malloc.h \
+ /home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/alloca.h \
+ /home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/strings.h \
+ /home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/types.h \
+ /home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/stdint.h \
+ /home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ /home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/machine/_types.h \
+ /home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/linux/posix_types.h \
+ /home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/linux/stddef.h \
+ /home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/linux/compiler.h \
+ /home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/asm/posix_types.h \
+ /home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/asm/types.h \
+ /home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/linux/types.h \
+ /home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/machine/kernel.h \
+ /home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h \
+ /home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/memory.h \
+ /home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/math.h \
+ /home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/limits.h \
+ /home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/limits.h \
+ /home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/linux/limits.h \
+ /home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/machine/internal_types.h \
+ /home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/machine/limits.h \
+ /home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/syslimits.h \
+ /home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/asm/page.h \
+ /home/omerjerk/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp \
+ /home/omerjerk/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/algorithm \
+ /home/omerjerk/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/cmath \
+ /home/omerjerk/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/cstddef \
+ /home/omerjerk/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/complex \
+ /home/omerjerk/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/map \
+ /home/omerjerk/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/vector \
+ /home/omerjerk/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp \
+ /home/omerjerk/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/limits \
+ /home/omerjerk/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp \
+ /home/omerjerk/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/highgui/highgui_c.h \
+ /home/omerjerk/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/core/core_c.h \
+ /home/omerjerk/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h \
+ /home/omerjerk/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/imgproc/imgproc.hpp \
+ /home/omerjerk/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/imgproc/types_c.h \
+ /home/omerjerk/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/video/tracking.hpp
+
+/home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/jni.h:
+
+/home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+
+/home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/android/api-level.h:
+
+/home/omerjerk/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/highgui/highgui.hpp:
+
+/home/omerjerk/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp:
+
+/home/omerjerk/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h:
+
+/home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/assert.h:
+
+/home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/stdlib.h:
+
+/home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/string.h:
+
+/home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/malloc.h:
+
+/home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/alloca.h:
+
+/home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/strings.h:
+
+/home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/types.h:
+
+/home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/stdint.h:
+
+/home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+
+/home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+
+/home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/linux/posix_types.h:
+
+/home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/linux/stddef.h:
+
+/home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/linux/compiler.h:
+
+/home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/asm/posix_types.h:
+
+/home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/asm/types.h:
+
+/home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/linux/types.h:
+
+/home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/machine/kernel.h:
+
+/home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h:
+
+/home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/memory.h:
+
+/home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/math.h:
+
+/home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/limits.h:
+
+/home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/limits.h:
+
+/home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/linux/limits.h:
+
+/home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/machine/internal_types.h:
+
+/home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/machine/limits.h:
+
+/home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/syslimits.h:
+
+/home/omerjerk/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/asm/page.h:
+
+/home/omerjerk/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp:
+
+/home/omerjerk/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/algorithm:
+
+/home/omerjerk/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/cmath:
+
+/home/omerjerk/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/cstddef:
+
+/home/omerjerk/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/complex:
+
+/home/omerjerk/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/map:
+
+/home/omerjerk/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/vector:
+
+/home/omerjerk/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp:
+
+/home/omerjerk/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/limits:
+
+/home/omerjerk/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp:
+
+/home/omerjerk/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/highgui/highgui_c.h:
+
+/home/omerjerk/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/core/core_c.h:
+
+/home/omerjerk/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h:
+
+/home/omerjerk/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/imgproc/imgproc.hpp:
+
+/home/omerjerk/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/imgproc/types_c.h:
+
+/home/omerjerk/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/video/tracking.hpp:
